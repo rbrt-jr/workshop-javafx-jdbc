82ab96d12be0f139db45f2f8273922eeb9bc0b22
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-05-10 21:12:45 +0100

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -22,6 +25,8 @@ public class DepartmentFormController implements Initializable {
 	private Department entity;
 
 	private DepartmentService service;
+
+	private List<DataChangeListener> dataChangeListerners = new ArrayList<>();
 
 	@FXML
 	private TextField txtId;
@@ -49,10 +54,18 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		} catch (DbException d) {
 			Alerts.showAlert("Error saving object", null, d.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener d : dataChangeListerners) {
+			d.onDataChanged();
+		}
+
 	}
 
 	private Department getFormData() {
@@ -71,6 +84,10 @@ public class DepartmentFormController implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
+	}
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListerners.add(listener);
 	}
 
 	public void setDepartmentService(DepartmentService service) {
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,7 +27,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable {
+public class DepartmentListController implements Initializable, DataChangeListener {
 
 	private DepartmentService service;
 	@FXML
@@ -81,10 +82,11 @@ public class DepartmentListController implements Initializable {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
+
 			DepartmentFormController dfc = loader.getController();
 			dfc.setDepartment(obj);
 			dfc.setDepartmentService(new DepartmentService());
+			dfc.subscribeDataChangeListener(this);
 			dfc.upDateFormData();
 
 			Stage dialogStage = new Stage();
@@ -98,6 +100,12 @@ public class DepartmentListController implements Initializable {
 			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+
 	}
 
 }
